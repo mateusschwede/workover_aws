5117214607435af2f823e4ca86d88f26d24a2fb6
mateusschwede <mateus1908.schwede@gmail.com>
2025-11-17 17:56:58 -0300

    3_app_runner (somente django)

--- 3_app_runner/paris_cafe/paris_cafe/settings.py
+++ 3_app_runner/paris_cafe/paris_cafe/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%i%(yjz++h$@pfcpma&c0qwluj7p24d0bwk5!+p#t1b_g*$hsc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -129,8 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
